08cfc058ecffa894592b76dceef03b9a5b9e1569
Erickson Quadros <68227253+Erickson-de-Quadros-s@users.noreply.github.com>
2021-05-02 15:27:05 -0300

    Adicionar arquivos de projeto.

---
+++ projeto_esfera_raio/Calculadora.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+namespace projeto_esfera_raio
+{
+    class  Calculadora
+    {
+        public static double pi = 3.14;
+
+         public static double Circunferencia(double raio)
+        {
+            return 2.0 * pi * raio;
+
+        }
+        public static double Volume(double raio)
+        {
+            return 4.0 / 3.0 * pi * Math.Pow(raio, 3); // Pow coloca o raio elevado na 3
+        }
+    }
+}
---
+++ projeto_esfera_raio/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace projeto_esfera_raio
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor do raio!");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Calculadora.Circunferencia(raio);
+            Console.WriteLine("A Circunferencia eh \n" + circ.ToString("F2", CultureInfo.InvariantCulture));
+
+            double volume = Calculadora.Volume(raio);
+            Console.WriteLine("O Volume eh \n" + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("O Valor de PI eh \n" + Calculadora.pi.ToString("F2", CultureInfo.InvariantCulture));
+           
+        }
+    }
+
+}
